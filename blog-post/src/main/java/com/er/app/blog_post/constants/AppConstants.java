b34ddf43795e40cc1a10cd778be7fc7d195e770a
Eknath Reddy M <151753511+eknaathreddyM@users.noreply.github.com>
2024-12-14 20:04:51 +0530

    Delete blog-post directory

--- blog-post/src/main/java/com/er/app/blog_post/constants/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.er.app.blog_post.constants;
-
-public final class  AppConstants {
-    public static  final String DEFAULT_PAGE_NUMBER="0";
-    public static final  String DEFAULT_PAGE_SIZE="10";
-    public static final String DEFAULT_SORT_BY="id";
-    public static  final String DEFAULT_SORT_DIRECTION="ASC";
-}
--- blog-post/src/main/java/com/er/app/blog_post/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.er.app.blog_post.repository;
-
-import com.er.app.blog_post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-
-}
